52f09806f4435986f3de3f135688d9eac14767fa
krodrigues <kenneth.rodrigues@grupofolha.com.br>
2019-09-17 19:06:43 -0300

    first commit

---
+++ src/server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const routes = require('./routes');
+const server = express();
+
+mongoose.connect('mongodb+srv://teste1:teste1@clusterapi-luuvn.mongodb.net/omnistack2?retryWrites=true&w=majority', {useUnifiedTopology: true, useNewUrlParser: true});
+server.use(cors());
+server.use(express.json());
+server.use(routes);
+
+server.listen(3333);
